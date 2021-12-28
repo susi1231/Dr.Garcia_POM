55a70fdb7354675056b6aa2caa9d569615ab1f62
susi1231 <kanisirisusi@gmail.com>
2021-12-28 10:04:58 +0530

    python

---
+++ Config/Config.py
@@ -0,0 +1,21 @@
+
+class TestData:
+    CHROME_EXECUTABLE_PATH="C:\\Users\\kalyan\\Downloads\\chromedriver_win32 (1)\\driver\\chromedriver.exe"
+    FIREFOX_EXECUTABLE_PATH = "C:\\Users\\kalyan\\Downloads\\geckodriver-v0.30.0-win64 (1)\\geckodriver.exe"
+
+
+
+    BASE_URL = "http://54.85.128.201/login"
+    USER_NAME = "omkartest1234@gmail.com"
+    PASSWORD = "Sushi@123"
+    LOGIN_PAGE_TITLE = "Dr-Gracia"
+
+    NAME = "Sushi Test"
+    SIGNUP_USER_EMAIL = "omkartest12345@gmail.com"
+    PHONE_NUMBER = "7981867710"
+    AGE = "24"
+    SIGNUP_PASSWORD = "Sushi@123"
+    SIGNUP_CONFIRM_PASSWORD = "Sushi@123"
+    SIGNUP_CONFIRMATION_MESSAGE = ""
+    SIGNUP_PAGE_TITLE="Dr-Gracia"
+    HOMEPAGE_TITLE = "US BIOMAG | Homepage"
---
+++ Pages/PurchaseMainCourse.py
@@ -0,0 +1,48 @@
+from selenium.webdriver.common.by import By
+import time
+from Config.Config import TestData
+from Pages.Basepage import BasePage
+from Pages.CoursePage import CoursePage
+from Pages.LandingPage import LandingPage
+from Pages.Payment import Payment
+
+
+class Purchase_Main_Course(BasePage):
+    """By_locators== Object repository"""
+    NAME = (By.XPATH, "//input[@id='InputName']")
+    EMAIL = (By.XPATH, "//input[@id='InputEmail']")
+    PHONE = (By.XPATH, "//input[@id='InputPhone']")
+    COUNTRY = (By.XPATH, "//input[@id='InputCountry']")
+    STATE = (By.XPATH, "//input[@id='InputState']")
+    CITY = (By.XPATH, "//input[@id='InputCity']")
+    ZIPCODE = (By.XPATH, "//input[@id='InputPincode']")
+    PROMOCODE = (By.XPATH, "//input[@id='InputPromocode']")
+    APPLY_PROMOCODE = (By.XPATH, "//input[@id='InputApplypromocode']")
+    CHECKBOX = (By.XPATH, "//input[@id='Check1']")
+    BUYBUTTON = (By.XPATH, "//button[.='Buy the Course']")
+
+    """constructor of the page class"""
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver.get(TestData.BASE_URL)
+
+    def purchase_course(self):
+        self.do_click(LandingPage.THREELINES)
+        self.do_click(LandingPage.COURSE)
+        self.do_click(CoursePage.APPLY_COURSE)
+        self.do_send_keys(self.COUNTRY, "India")
+        self.do_send_keys(self.STATE, "Karnataka")
+        self.do_send_keys(self.CITY, "Bangalore")
+        self.do_send_keys(self.ZIPCODE, "524002")
+        self.do_click(self.CHECKBOX)
+        self.do_click(self.BUYBUTTON)
+
+
+
+
+
+
+
+
+
+
